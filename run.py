1a3a78de9c96b88fa7ba991e70f45d8b4f9ad772
ATON_SOL <44841864+atonsol@users.noreply.github.com>
2024-12-18 16:21:45 +0700

    Update run.py

--- run.py
+++ run.py
@@ -1,58 +1,115 @@
-#script for 1 user_id and a whole bunch of proxies
 import asyncio
 import random
 import ssl
 import json
 import time
 import uuid
-import requests
-import shutil
-from loguru import logger
+import logging
+from datetime import datetime
+from colorama import init, Fore, Style
 from websockets_proxy import Proxy, proxy_connect
-from fake_useragent import UserAgent
 
-user_agent = UserAgent(os='windows', platforms='pc', browsers='chrome')
-random_user_agent = user_agent.random
+init(autoreset=True)
+
+CHROME_USERAGENTS = [
+    "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/122.0.0.0 Safari/537.36",
+    "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/121.0.0.0 Safari/537.36",
+    "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36"
+]
+
+EDGE_USERAGENTS = [
+    "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/122.0.0.0 Safari/537.36 Edg/122.0.2365.57",
+    "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/121.0.0.0 Safari/537.36 Edg/121.0.2277.83",
+    "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36 Edg/120.0.2210.91"
+]
+
+def colorful_log(proxy, device_id, message_type, message_content, is_sent=False):
+    timestamp = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+    
+    color = Fore.GREEN if is_sent else Fore.BLUE
+    action_color = Fore.YELLOW
+    
+    log_message = (
+        f"{Fore.WHITE}[{timestamp}] "
+        f"{Fore.MAGENTA}[Proxy: {proxy}] "
+        f"{Fore.CYAN}[Device ID: {device_id}] "
+        f"{action_color}[{message_type}] "
+        f"{color}{message_content}"
+    )
+    
+    print(log_message)
 
 async def connect_to_wss(socks5_proxy, user_id):
-    device_id = str(uuid.uuid3(uuid.NAMESPACE_DNS, socks5_proxy))
-    logger.info(f"Connecting with Device ID: {device_id}")
+    device_id = str(uuid.uuid5(uuid.NAMESPACE_DNS, socks5_proxy))
+    
+    random_user_agent = random.choice(CHROME_USERAGENTS) if hash(socks5_proxy) % 2 == 0 else random.choice(EDGE_USERAGENTS)
+
+    colorful_log(
+        proxy=socks5_proxy,  
+        device_id=device_id, 
+        message_type="INIT", 
+        message_content=f"User Agent: {random_user_agent}"
+    )
+
     while True:
         try:
             await asyncio.sleep(random.randint(1, 10) / 10)
             custom_headers = {
                 "User-Agent": random_user_agent,
-                #Node 1.25x but maintenance, not connected!
-                #"Origin": "chrome-extension://lkbnfiajjmbhnfledhphioinpickokdi"
-                
-                #Lite 1x but run normal
-                "Origin": "chrome-extension://ilehaonighjijnmpnagapkhpcdbhclfg"
+                "Origin": "chrome-extension://lkbnfiajjmbhnfledhphioinpickokdi"
             }
             ssl_context = ssl.create_default_context()
             ssl_context.check_hostname = False
             ssl_context.verify_mode = ssl.CERT_NONE
-            urilist = ["wss://proxy.wynd.network:4444/","wss://proxy.wynd.network:4650/"]
+            
+            urilist = [
+                #"wss://proxy.wynd.network:4444/",
+                #"wss://proxy.wynd.network:4650/",
+                "wss://proxy2.wynd.network:4444/",
+                "wss://proxy2.wynd.network:4650/",
+                #"wss://proxy3.wynd.network:4444/",
+                #"wss://proxy3.wynd.network:4650/"
+            ]
+            
             uri = random.choice(urilist)
-            #uri = "wss://proxy.wynd.network:4650/"
             server_hostname = "proxy.wynd.network"
             proxy = Proxy.from_url(socks5_proxy)
+            
             async with proxy_connect(uri, proxy=proxy, ssl=ssl_context, server_hostname=server_hostname,
                                      extra_headers=custom_headers) as websocket:
                 async def send_ping():
                     while True:
                         send_message = json.dumps(
-                            {"id": str(uuid.uuid4()), "version": "1.0.0", "action": "PING", "data": {}})
-                        logger.debug(f"Sending PING: {send_message}")
+                            {"id": str(uuid.uuid5(uuid.NAMESPACE_DNS, socks5_proxy)), 
+                             "version": "1.0.0", 
+                             "action": "PING", 
+                             "data": {}})
+                        
+                        colorful_log(
+                            proxy=socks5_proxy,  
+                            device_id=device_id, 
+                            message_type="SEND PING", 
+                            message_content=send_message,
+                            is_sent=True
+                        )
+                        
                         await websocket.send(send_message)
                         await asyncio.sleep(5)
-                # asyncio.create_task(send_http_request_every_10_seconds(socks5_proxy, device_id))
+
                 await asyncio.sleep(1)
                 asyncio.create_task(send_ping())
 
                 while True:
                     response = await websocket.recv()
                     message = json.loads(response)
-                    logger.info(f"Received message: {message}")
+                    
+                    colorful_log(
+                        proxy=socks5_proxy, 
+                        device_id=device_id, 
+                        message_type="RECEIVE", 
+                        message_content=json.dumps(message)
+                    )
+
                     if message.get("action") == "AUTH":
                         auth_response = {
                             "id": message["id"],
@@ -60,36 +117,57 @@ async def connect_to_wss(socks5_proxy, user_id):
                             "result": {
                                 "browser_id": device_id,
                                 "user_id": user_id,
-                                "user_agent": custom_headers['User-Agent'],
+                                "user_agent": random_user_agent,
                                 "timestamp": int(time.time()),
                                 "device_type": "extension",
                                 "version": "4.26.2",
-                                "extension_id": "ilehaonighjijnmpnagapkhpcdbhclfg"
+                                "extension_id": "lkbnfiajjmbhnfledhphioinpickokdi"
                             }
                         }
-                        logger.debug(f"Sending AUTH response: {auth_response}")
+                        
+                        colorful_log(
+                            proxy=socks5_proxy,  
+                            device_id=device_id, 
+                            message_type="SEND AUTH", 
+                            message_content=json.dumps(auth_response),
+                            is_sent=True
+                        )
+                        
                         await websocket.send(json.dumps(auth_response))
 
                     elif message.get("action") == "PONG":
                         pong_response = {"id": message["id"], "origin_action": "PONG"}
-                        logger.debug(f"Sending PONG response: {pong_response}")
+                        
+                        colorful_log(
+                            proxy=socks5_proxy, 
+                            device_id=device_id, 
+                            message_type="SEND PONG", 
+                            message_content=json.dumps(pong_response),
+                            is_sent=True
+                        )
+                        
                         await websocket.send(json.dumps(pong_response))
         except Exception as e:
-            logger.error(f"Error with proxy {socks5_proxy}: {e}")
-
+            colorful_log(
+                proxy=socks5_proxy, 
+                device_id=device_id, 
+                message_type="ERROR", 
+                message_content=str(e)
+            )
+            await asyncio.sleep(5)
 
 async def main():
-    #find user_id on the site in conlose localStorage.getItem('userId') (if you can't get it, write allow pasting)
-    #user_ids
+    print(f"{Fore.CYAN}ATON | GetGrass Crooter V2{Style.RESET_ALL}")
     with open('usergrass.txt', 'r') as file:
-            user_ids = file.read().splitlines()
-        
+        _user_id = file.read().splitlines()
+    
     with open('proxy_list.txt', 'r') as file:
-            local_proxies = file.read().splitlines()
-        
-    tasks = [asyncio.ensure_future(connect_to_wss(i, user_ids)) for i in local_proxies]
+        local_proxies = file.read().splitlines()
+    
+    print(f"{Fore.YELLOW}Total Proxies: {len(local_proxies)}{Style.RESET_ALL}")
+    
+    tasks = [asyncio.ensure_future(connect_to_wss(i, _user_id)) for i in local_proxies]
     await asyncio.gather(*tasks)
 
 if __name__ == '__main__':
-    #letsgo
     asyncio.run(main())
